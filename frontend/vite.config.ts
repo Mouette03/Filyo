3bddbe1ba47c74e27ee277ef9dcde8f353dd76b1
Damien <mouette03@yahoo.fr>
2026-04-01 21:25:02 +0200

    test

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import { readFileSync } from 'fs'
 
-const { version } = JSON.parse(readFileSync(new URL('../backend/package.json', import.meta.url), 'utf-8'))
+const { version } = JSON.parse(readFileSync(new URL('./package.json', import.meta.url), 'utf-8'))
 
 export default defineConfig({
   plugins: [react()],
