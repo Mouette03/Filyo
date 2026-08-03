7adbe8623cd5aab6b25c2ff644125e05ae5d3dd5
Damien <mouette03@yahoo.fr>
2026-08-03 22:13:29 +0200

    Update App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,4 @@
-import { BrowserRouter } from 'react-router/dom'
-import { Routes, Route } from 'react-router'
+import { BrowserRouter, Routes, Route } from 'react-router'
 import { Toaster } from 'react-hot-toast'
 import { useEffect } from 'react'
 import { usePreferencesStore, applyTheme, applyAccent, applyBgColor, resetBgColor } from './stores/usePreferencesStore'
